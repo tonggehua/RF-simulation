0275ba1e8250ed030367aa60a680c9d5886be827
Gehua Tong <31249056+tonggehua@users.noreply.github.com>
2022-03-09 20:19:35 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from typing import Tuple, Union
+import setuptools
+
+
+setuptools.setup(
+    author="Gehua Tong",
+    author_email="gt2407@columbia.edu",
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+    ],
+    description="MRI RF Simulation",
+    include_package_data=True,
+    install_requires=[
+        'matplotlib>=3.4.2',
+        'numpy>=1.21.0',
+        'scipy>=1.7.0',
+        'pypulseq==1.3.1.post1'
+    ],
+    license='License :: OSI Approved :: GNU Affero General Public License v3',
+    name="RF-simulation",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.9.0',
+    url="https://github.com/tonggehua/RF-simulation",
+    version="1.0.0",
+)
